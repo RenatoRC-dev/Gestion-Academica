42c368a85856f9599b50a9ae67c247fe2308c560
Renato Rebollo <renatorebollocondori@gmail.com>
2025-10-27 22:16:00 -0400

    chore: remover backend_wip del repositorio (mantener local)

--- backend_wip/bootstrap/providers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    Illuminate\Foundation\Providers\FoundationServiceProvider::class,
-    Illuminate\Foundation\Providers\FormRequestServiceProvider::class,
-];
